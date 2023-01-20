9273dfd1b582ae7ab80200c4f1226d735b6e398d
Erick Peneluca <erickpeneluca@gmail.com>
2023-01-20 01:24:07 -0300

    build

---
+++ src/Api.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-template-curly-in-string */
+const API_KEY = "e71661ed2f06258920aa97353ddc6bbb";
+
+const categories = [{
+        name: "trending",
+        title: "Em alta",
+        path: `/trending/all/week?api_key=${API_KEY}&language=pt-BR`,
+        isLarge: true,
+} ,
+{
+        name: "netflixOriginals",
+        title: "Originais Netflix",
+        path: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+        isLarge: false,
+
+},
+{
+        name: "topRated",
+        title: "Populares",
+        path: `/movie/top_rated?api_key=${API_KEY}&language=pt-BR`,
+        isLarge: false,
+},
+{
+        name: "comedy",
+        title: "Comédias",
+        path: `/trending/all/week?api_key=${API_KEY}&with_genres=35`,
+        isLarge: false,
+},
+{
+        name: "romances",
+        title: "Romances",
+        path: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+        isLarge: false,
+},
+{
+        name: "documentaries",
+        title: "Documentários",
+        path: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+        isLarge: false,
+}];
+
+export const getMovies = async (path) =>{
+    try{
+        let url = `Https://api.themoviedb.org/3/${path}`;
+        const response = await fetch(url);
+        return await response.json();
+    } catch(error){
+        console.log("Error getMovies: ", error)
+    }
+};
+
+export default categories;
